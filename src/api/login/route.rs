e5cbaec29df465a9e0c09ce9e8c552cfe97bf94e
DaeHan Kim <kjs50458281@gmail.com>
2024-02-06 17:55:25 +0900

    .

--- src/api/login/route.rs
+++ src/api/login/route.rs
@@ -1,17 +1,6 @@
-use crate::api::post::handler;
+use crate::api::login::handler;
 use actix_web::web;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("")
-            .route(web::get().to(handler::get_posts))
-            .route(web::post().to(handler::create_posts))
-            .route(web::put().to(handler::update_posts)),
-    );
-
-    cfg.service(
-        web::resource("/{id}")
-            .route(web::get().to(handler::get_posts_by_id))
-            .route(web::delete().to(handler::delete_posts_by_id)),
-    );
+    cfg.service(web::resource("/google").route(web::get().to(handler::google_oauth_handler)));
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,11 +1,11 @@
 use actix_web::{http::header::ContentType, web, HttpResponse, Responder};
 
-use crate::api::post::route;
+use crate::api::{login, post};
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/").route(web::get().to(my_handler)));
-    // /posts 서브 라우터를 추가
-    cfg.service(web::scope("/posts").configure(route::configure));
+    cfg.service(web::scope("/posts").configure(post::route::configure));
+    cfg.service(web::scope("/login").configure(login::route::configure));
 }
 
 async fn my_handler() -> impl Responder {
