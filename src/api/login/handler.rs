41e52ee8eb7759a07e74d58901517d07e92a8aa3
DaeHan Kim <kjs50458281@gmail.com>
2024-02-13 19:16:17 +0900

    .

--- src/api/login/handler.rs
+++ src/api/login/handler.rs
@@ -1,15 +1,54 @@
 use super::error::PostError;
+use super::model::QueryCode;
+use crate::api::login::model::{get_google_user, request_token};
 use crate::database::PgPool;
 use actix_web::Responder;
-use actix_web::{http::header::ContentType, web, web::Data, HttpRequest, HttpResponse, Result};
-use log::{error, info, warn};
-use serde_json::to_vec;
+use actix_web::{
+    web::{Data, Query},
+    HttpResponse, Result,
+};
+use log::{info, warn};
+use reqwest::header::LOCATION;
 
-pub async fn google_oauth_handler(pool: Data<PgPool>) -> Result<impl Responder, PostError> {
+pub async fn google_oauth_handler(
+    query: Query<QueryCode>,
+    pool: Data<PgPool>,
+) -> Result<impl Responder, PostError> {
     info!("로깅 테스트");
     warn!("로깅 테스트2");
 
-    Ok(HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .body("google"))
+    let code = &query.code;
+    let state = &query.state;
+
+    println!("{:?} {:?}", code, state);
+
+    if code.is_empty() {
+        return Ok(HttpResponse::Unauthorized().json(
+            serde_json::json!({"status": "fail", "message": "Authorization code not provided!"}),
+        ));
+    }
+
+    let token_response = request_token(code.as_str()).await;
+    if token_response.is_err() {
+        let message = token_response.err().unwrap().to_string();
+        return Ok(HttpResponse::BadGateway()
+            .json(serde_json::json!({"status": "fail", "message": message})));
+    }
+
+    let token_response = token_response.unwrap();
+    let google_user = get_google_user(&token_response.access_token, &token_response.id_token).await;
+    if google_user.is_err() {
+        let message = google_user.err().unwrap().to_string();
+        return Ok(HttpResponse::BadGateway()
+            .json(serde_json::json!({"status": "fail", "message": message})));
+    }
+
+    let google_user = google_user.unwrap();
+
+    println!("{:?}", google_user);
+
+    let frontend_origin = "http://localhost:3000";
+    let mut response = HttpResponse::Found();
+    response.append_header((LOCATION, format!("{}{}", frontend_origin, state)));
+    Ok(response.finish())
 }
--- src/api/login/model.rs
+++ src/api/login/model.rs
@@ -1,109 +1,74 @@
-use crate::database::PgPool;
-use crate::schema::posts::{self, dsl::*};
-use actix_web::web::Data;
-use chrono::Utc;
-use diesel::prelude::*;
-use diesel::{result::Error, AsChangeset, Insertable, Queryable};
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
+use reqwest::{Client, Url};
+use serde::Deserialize;
+use std::{env, error::Error};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Queryable)]
-#[diesel(table_name = crate::schema::posts)]
-pub struct Post {
+#[derive(Deserialize)]
+pub struct OAuthResponse {
+    pub access_token: String,
+    pub id_token: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GoogleUserResult {
     pub id: String,
-    pub title: String,
-    pub body: String,
-    pub published: bool,
-    pub created_at: Option<chrono::NaiveDateTime>,
-    pub updated_at: Option<chrono::NaiveDateTime>,
+    pub email: String,
+    pub verified_email: bool,
+    pub name: String,
+    pub given_name: String,
+    pub family_name: String,
+    pub picture: String,
+    pub locale: String,
 }
 
-// <'a> 은 라이프타임 매개변수를 나타냄(a라는 라이프타임이 있다)
-#[derive(Serialize, Deserialize, Debug, Clone, Insertable, AsChangeset)]
-#[diesel(table_name = crate::schema::posts)]
-pub struct PostData {
-    pub id: Option<String>,
-    pub title: String,
-    pub body: String,
-    pub published: Option<bool>,
-    pub updated_at: Option<chrono::NaiveDateTime>,
+#[derive(Debug, Deserialize)]
+pub struct QueryCode {
+    pub code: String,
+    pub state: String,
 }
-// Query (조회)할 때 (Queryable):
 
-// 조회된 데이터는 데이터베이스에서 읽어오는 것이기 때문에 소유권을 가질 필요가 있습니다. 그래서 String과 같이 소유권을 가지는 타입을 사용합니다.
-// String은 동적으로 크기가 조절되는 문자열을 나타내며, 이는 데이터베이스에서 읽어온 문자열의 크기가 불확실할 수 있기 때문에 적합합니다.
+pub async fn request_token(authorization_code: &str) -> Result<OAuthResponse, Box<dyn Error>> {
+    let redirect_url = env::var("REDIRECT_URL").expect("REDIRECT_URL must be set");
+    let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET must be set");
+    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID must be set");
 
-// Insert (삽입)할 때 (Insertable):
+    let root_url = "https://oauth2.googleapis.com/token";
+    let client = Client::new();
 
-// 데이터를 데이터베이스에 넣을 때는 소유권을 넘기는 것이 아니라 참조만 넘기는 것이 효율적일 수 있습니다. 특히 문자열을 데이터베이스에 넣을 때 크기를 미리 알 수 없는 경우가 많기 때문에 동적인 문자열을 소유하기보다는 참조를 사용하는 것이 좋습니다.
-// 따라서 &'a str과 같이 라이프타임이 있는 참조를 사용하여 문자열을 참조하고, 데이터베이스에는 참조만 전달합니다.
+    let params = [
+        ("grant_type", "authorization_code"),
+        ("redirect_uri", redirect_url.as_str()),
+        ("client_id", client_id.as_str()),
+        ("code", authorization_code),
+        ("client_secret", client_secret.as_str()),
+    ];
+    let response = client.post(root_url).form(&params).send().await?;
 
-impl Post {
-    pub async fn get_posts(
-        pool: &Data<PgPool>,
-    ) -> Result<Vec<(String, String, String, bool)>, Error> {
-        let conn = &mut pool.get().expect("Couldn't get DB connection from pool");
-
-        posts
-            .select((body, title, posts::id, published))
-            .load::<(String, String, String, bool)>(conn)
-    }
-
-    pub async fn get_posts_load(pool: &Data<PgPool>) -> Result<Vec<Post>, Error> {
-        let conn = &mut pool.get().expect("Couldn't get DB connection from pool");
-        // use crate::schema::posts::{dsl::*}로 인해서 posts::table을 posts로 사용가능
-        posts.load::<Post>(conn)
-    }
-
-    pub async fn get_posts_by_id(
-        post_id: &str,
-        pool: &Data<PgPool>,
-    ) -> Result<(String, String, String, bool), Error> {
-        let conn = &mut pool.get().expect("Couldn't get DB connection from pool");
-        posts
-            .find(post_id)
-            // .filter(posts::id.eq(post_id))
-            .select((body, title, posts::id, published))
-            // get_result: 주어진 조건에 해당하는 하나의 결과를 반환, 결과가 여러 개거나 없으면 에러(정확히 하나의 결과가 예상되는 상황)
-            .get_result::<(String, String, String, bool)>(conn)
-        // first: 조건에 해당하는 모든 결과 중 첫 번째 결과 반환
-        // .first::<(String, String, String, bool)>(conn)
-        // load: 여러 레코드를 로드하고 벡터로 반환, 결과를 단일 값이 아닌 여러 레코드로 받아오려 할 때 사용
-        // .load::<(String, String, String, bool)>(conn)
-    }
-
-    pub async fn delete_posts_by_id(post_id: &str, pool: &Data<PgPool>) -> Result<usize, Error> {
-        let conn = &mut pool.get().expect("Couldn't get DB connection from pool");
-        diesel::delete(posts.find(post_id)).execute(conn)
+    if response.status().is_success() {
+        let oauth_response = response.json::<OAuthResponse>().await?;
+        Ok(oauth_response)
+    } else {
+        let message = "An error occurred while trying to retrieve access token.";
+        Err(From::from(message))
     }
 }
 
-impl PostData {
-    pub async fn create_posts(post_data: PostData, pool: &Data<PgPool>) -> Result<(), Error> {
-        let post = PostData {
-            id: Some(Uuid::new_v4().to_string()),
-            ..post_data
-        };
+pub async fn get_google_user(
+    access_token: &str,
+    id_token: &str,
+) -> Result<GoogleUserResult, Box<dyn Error>> {
+    let client = Client::new();
+    let mut url = Url::parse("https://www.googleapis.com/oauth2/v1/userinfo").unwrap();
+    url.query_pairs_mut().append_pair("alt", "json");
+    url.query_pairs_mut()
+        .append_pair("access_token", access_token);
 
-        let conn = &mut pool.get().expect("Couldn't get DB connection from pool");
+    let response = client.get(url).bearer_auth(id_token).send().await?;
 
-        diesel::insert_into(posts).values(post).execute(conn)?;
-        Ok(())
-    }
-
-    pub async fn update_posts(post_data: PostData, pool: &Data<PgPool>) -> Result<usize, Error> {
-        let conn = &mut pool.get().expect("Couldn't get DB connection from pool");
-        let updated_date = Some(Utc::now().naive_utc());
-
-        let post = PostData {
-            id: None,
-            updated_at: updated_date,
-            ..post_data
-        };
-
-        diesel::update(posts.find(post_data.id.unwrap()))
-            .set(post)
-            // .get_result::<Post>(conn)
-            .execute(conn)
+    if response.status().is_success() {
+        let user_info = response.json::<GoogleUserResult>().await?;
+        Ok(user_info)
+    } else {
+        let message = "An error occurred while trying to retrieve user information.";
+        Err(From::from(message))
     }
 }
