22162802091ed0e725f9f85d3a14be509ef94fd9
DaeHan Kim <kjs50458281@gmail.com>
2024-02-17 19:47:22 +0900

    [FEAT]: add users

--- src/schema.rs
+++ src/schema.rs
@@ -12,3 +12,28 @@ diesel::table! {
         updated_at -> Nullable<Timestamp>,
     }
 }
+
+diesel::table! {
+    users (id) {
+        #[max_length = 255]
+        id -> Varchar,
+        #[max_length = 255]
+        google_id -> Varchar,
+        #[max_length = 255]
+        email -> Varchar,
+        #[max_length = 255]
+        user_name -> Varchar,
+        verified -> Nullable<Bool>,
+        #[max_length = 255]
+        provider -> Varchar,
+        #[max_length = 255]
+        photo -> Varchar,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
