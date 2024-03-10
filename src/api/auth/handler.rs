a923308308689e453f39ed84e27f28b50a6e5708
DaeHan Kim <kjs50458281@gmail.com>
2024-03-10 17:16:01 +0900

    fix: add comment

--- src/api/auth/handler.rs
+++ src/api/auth/handler.rs
@@ -31,6 +31,7 @@ pub async fn google_oauth_handler(
             serde_json::json!({"status": "fail", "message": "Authorization code not provided!"}),
         ));
     }
+    println!("code: {:?}", query);
 
     let token_response = request_token(code.as_str()).await;
     if token_response.is_err() {
@@ -164,3 +165,5 @@ pub async fn logout_handler(_: AuthenticationGuard) -> impl Responder {
         .cookie(cookie)
         .json(serde_json::json!({"status": "success"}))
 }
+
+// https://accounts.google.com/signin/oauth/id?authuser=1&part=AJi8hAOV3bDJfjMiWxZmMBX3UHRjlEKNLVITKzcfmxdm-Y3p_IXWszqDypzr2xGinr-iPDy2jTvPkneDxJK-se_YokHAbtSvgTAp0qOH6NSJLvNcwCi0suxEx7I1Z8EDuq6QKPVWG5W-2tnvAqAW6KhWYFoGEa1stt9Ded6vLvMNUdw8TcxmwVVtH5JItRWFrM2xgA1SrqpNm82-mh9cCdnc3kMz7nFXUGjkAqyskn8NQfhT20QJriCQAQXPkQSoTXcXRxKMwMT1pCuSIsEZXUFLDDPiPY5fue7ka2xXAsk6U765SJXLm8BFwTls8uUJJP799ciTGdmuAA0bekTRkRbQLVrIV_sw2mGgUqkDUKGtNjdYj3YcZE-yNvOOmH8KuqaPgoM9VtoQWQs2hWz7QK98V66Jh2_kJtY_J-KTuO2RcEjj0mggLH03CJ8BB616Fua8cFHXIWMULO9ri3y4gSGPikHAmWUwEX0nxMaNOkZ977lrvbb_GBZsC04QAINrUHZBd1-p0Oqp9EA9R_gfxlTNAujFRdoQ20n5vFxw5bDiz-z1jZVUvx-HePALm28vnU1qOxOX4GUgDlbe5uCrAKtyPkWuu8V6ZgMHSESx4-UQ8LzWUaSQjsu6q_S5QBnFx5HJPh9mQNVe0lU83ddhXnbH26a1GP3vdOavFRm4vK6m83xsP1UQEowJk1nNBIh1O-sRzdX_5c2qFiYpZ4mbIMw2ZXS7VjBlo_jBtMRoTmqfOoKEANsoa1WRQCkjTr8RVH1m9iwfhwdG5QN1KbtOTJozWHaSSY31UhyWsbHppHtx8jMC0tvtOqqb2vke7BdDoUEOIsjAXhaeggFoEkDr-_UR82a3X5eWVPORIBJrwtog9I8nZzQivn03jCAqeEnFk25OQYaaGh7nss0x1ZSELJXPytt4vIlusdMlmCkFAquVAfqaYicQFa9I5uMbTuAfaEvDKq3l4pCAXPJ-P9mhvT9A21izkiyxTvnvpvCfsHjuHdqs0B4w_GdE3l12tPsK-8HJaArgdH1jivpLhiZaAJ2SRH78R3e0fcnwEn-lAR3g1Oo9tFO9iYEMt51eu0H6fn8-8QipWZOrhJmNGsntF9nKPG6rm84z_bk00y-XKGLN9b7cvrBza3M&flowName=GeneralOAuthFlow&as=S2098109018%3A1709439746845592&client_id=296653457840-1ckfc04i9jsfgc20sm8bglfkbg592alg.apps.googleusercontent.com&rapt=AEjHL4PVehYZNktenBMyKZIGp_qV5pdgX6MzwRkgpv2-6NJ8q_S5Zl1UbLbkFO7SstAWLxLeg-OoxMLhAgVDLOUaT0RZ3RGQ9w&theme=glif#
