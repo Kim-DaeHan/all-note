494548b44f3ef5125f2ffdc9f537755941957037
DaeHan Kim <kjs50458281@gmail.com>
2024-03-24 11:26:20 +0900

    .

--- src/api/auth/model.rs
+++ src/api/auth/model.rs
@@ -17,7 +17,7 @@ pub struct GoogleUserResult {
     pub given_name: String,   // 사용자의 이름
     pub family_name: String,  // 사용자의 성
     pub picture: String,      // 사용자의 프로필 사진 URL
-                              // pub locale: String,       // 사용자의 지역 설정
+    pub locale: String,       // 사용자의 지역 설정
 }
 
 #[derive(Debug, Serialize, Deserialize)]
